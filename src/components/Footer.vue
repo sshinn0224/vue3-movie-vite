4977d5dac03a7149f9f5a76dd94a92a7cf2f4914
sshinn0224 <sshinn0224@gmail.com>
2023-09-06 06:46:29 +0900

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <Logo></Logo>
+    <a
+      href="https://github.com/sshinn0224"
+      target="_blank"
+      >(c){{ new Date().getFullYear() }} sshinn0224</a
+    >
+  </footer>
+</template>
+
+<script>
+import Logo from '@/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: 0.3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,7 @@ export default {
   namespaced: true,
   state: () => ({
     movies: [],
-    message: '',
+    message: 'Search For The Movie Title',
     loading: false,
   }),
   getters: {},
@@ -21,35 +21,62 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const { title, type, year, number } = payload
-      const OMDB_API_KEY = '7035c60c'
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-      )
-      const { Search, totalResults } = res.data
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID'),
-      })
-      console.log(totalResults)
-      console.log(typeof totalResults)
+      if (state.loading) return
 
-      const total = parseInt(totalResults, 10)
-      const pageLength = Math.ceil(total / 10)
+      commit('updateState', { message: '', loading: true })
+      try {
+        const res = await _fetchMovie({ ...payload, page: 1 })
 
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1) {
-          if (page > number / 10) break
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID'),
+        })
+        console.log(totalResults)
+        console.log(typeof totalResults)
 
-          const res = await axios.get(
-            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-          )
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
 
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
-          })
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > payload.number / 10) break
+
+            const res = await _fetchMovie({ ...payload, page })
+
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
+            })
+          }
         }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message,
+        })
+      } finally {
+        commit('updateState', { loading: false })
       }
     },
   },
+}
+
+function _fetchMovie(payload) {
+  const OMDB_API_KEY = '7035c60c'
+  const { title, type, year, page } = payload
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.Error) {
+          reject(res.data.Error)
+        }
+        resolve(res)
+      })
+      .catch((err) => {
+        reject(err.message)
+      })
+  })
 }
